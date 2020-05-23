8b77ebbe3c2ebd5b873ef615e82ed34e8033da21
Chirag Borawake <chiragdborawake@gmail.com>
2020-05-23 22:37:43 +0530

    Show time function added

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,9 @@
 function printHelloWorld() {
 	console.log('Hello world..!');
+	dispalyTime();
+}
+function displayTime(){
+	var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	console.log('Current Time:',time);
 }
